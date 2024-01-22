a9ac040a9892e48b8236afd87422ee6bf55bbb09
GabrielPerera <gabrielpereira0907@gmail.com>
2024-01-22 13:54:01 -0300

    resolução da segunda questão da atividade

---
+++ ManipuladorNumeros.java
@@ -0,0 +1,10 @@
+public class ManipuladorNumeros {
+    private int a;
+    private int calcular;
+
+    public void elevarAoQuadrado(int a){
+        this.a = a;
+        calcular = a*a;
+        System.out.println(calcular);
+    }
+}
